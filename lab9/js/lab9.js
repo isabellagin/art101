bf4d19f51888936b3bc13dae56b54f3672164b64
isabellagin <isivrodr@ucsc.edu>
2023-05-11 12:43:18 -0700

    Lab 9
    This is my lab

---
+++ lab9/js/lab9.js
@@ -0,0 +1,24 @@
+//index.js- Lab 9 - JavaScript for the Web
+// Author: Isabella Ginnold
+// Date: May 11, 2023
+
+//Constants
+
+//Functions
+
+//use getElementById() to find your output and assign it to outputEl
+  const outputEl = document.getElementById("output");
+  //outputEl.innerHTML = "p"; TESTING
+
+
+  const newIEl = document.createElement("p");
+  new1El.innerHTML = "Why don't we all just eat Vegan ice cream and enjoy the show";
+
+
+  const new2El = document.createElement("p");
+  new2El.innerHTML = "But what if I don't care if I'm lactosee intollerant?"
+
+  outputEl.appendChild(new1El);
+  outputEl.appendChild(new2El);
+
+  function myFunction(param1, param2)
